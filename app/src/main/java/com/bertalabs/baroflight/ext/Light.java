b8070391a7b7dbe71ab2afa105df6e8e0dfb0293
Jose Miguel Cruz Toledo <1273555+jctoledo@users.noreply.github.com>
2021-02-13 17:10:30 -0800

    light has temp and health now

--- app/src/main/java/com/bertalabs/baroflight/ext/Light.java
+++ app/src/main/java/com/bertalabs/baroflight/ext/Light.java
@@ -1,6 +1,5 @@
 package com.bertalabs.baroflight.ext;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -10,6 +9,9 @@ public class Light {
     public int request_power;
     public int actual_power;
     public int rssi;
+    public double temperature;
+    public boolean health;
+    public boolean isConnected;
     public LightGroup group;
     public LocalDateTime lastSeen;
 
@@ -19,15 +21,17 @@ public class Light {
         FILL;
     }
 
-    public Light(String ip_address, String mac_address, int request_power, int actual_power,
+    public Light(String ip_address, String mac_address, double temperature, int request_power, int actual_power,
                  int rssi, LocalDateTime lastSeen, LightGroup group) {
         this.ip_address = ip_address;
         this.mac_address = mac_address;
         this.request_power = request_power;
         this.actual_power = actual_power;
         this.rssi = rssi;
+        this.temperature = temperature;
         this.group = group;
         this.lastSeen = lastSeen;
+        this.isConnected = true;
     }
 
     @Override
@@ -38,6 +42,7 @@ public class Light {
         return request_power == light.request_power &&
                 actual_power == light.actual_power &&
                 rssi == light.rssi &&
+                temperature == light.temperature &&
                 group == light.group &&
                 lastSeen.equals(light.lastSeen) &&
                 ip_address.equals(light.ip_address) &&
@@ -46,13 +51,8 @@ public class Light {
 
     @Override
     public int hashCode() {
-        return Objects.hash(ip_address, mac_address, request_power, actual_power, rssi,
-                lastSeen, group);
+        return Objects.hash(ip_address, mac_address, request_power, actual_power, temperature,
+                rssi, lastSeen, group);
     }
-
-    private boolean isHot() {
-        return request_power != actual_power;
-    }
-
 
 }
